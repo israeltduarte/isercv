d59bf8d336c5a75f4a90abb8cd742ff11598dc44
Israel Terra Duarte <israeltduarte@Israels-MacBook-Pro.local>
2021-09-09 23:40:06 -0300

    first commit

---
+++ src/main/java/br/com/iser/isercv/IsercvApplication.java
@@ -0,0 +1,13 @@
+package br.com.iser.isercv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsercvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsercvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/iser/isercv/IsercvApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.iser.isercv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsercvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
